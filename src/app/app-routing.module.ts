77230d6bd4255faee0be16c1b0b104de67e346fd
Yecksin <yecksin@gmail.com>
2020-10-18 19:31:41 -0500

    Instalamos materialize, modulos de three js, loader y control

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'path2', component: HomeComponent },
+  { path: 'juego', component: GameComponent },
   { path: 'path3', component: HomeComponent },
   { path: 'path4', component: HomeComponent },
   { path: '**', component: HomeComponent },
